43e4dcaba78746de1953e6562bba13b51cca8de4
jafernaqvi <jafernaqvi01@gmail.com>
2020-08-11 23:44:38 +0200

    simple course api based on sprint boot

---
+++ src/main/java/com/company/CourseApiApp.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+    public static void main(String[] args) {
+	// write your code here
+        SpringApplication.run(CourseApiApp.class,args);
+    }
+}
